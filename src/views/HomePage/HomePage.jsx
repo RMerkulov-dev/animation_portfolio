e9300588f639d9343fa34b74fa0d2933869f509e
RMerkulov <fotoromario@gmail.com>
2023-03-01 13:39:21 +0200

    upd

--- src/views/HomePage/HomePage.jsx
+++ src/views/HomePage/HomePage.jsx
@@ -17,8 +17,57 @@ const HomePage = () => {
       <section className={s.portfolioSection}>
         <Container>
           <div className={s.portfolioContent}>
-            <h2>Portfolio</h2>
+            <h2 className={s.sectionTitle}>Portfolio</h2>
             <SliderMobile />
+          </div>
+        </Container>
+      </section>
+      <section className={s.aboutSection}>
+        <Container>
+          <div className={s.aboutContent}>
+            <h2 className={s.sectionTitle}>About This Gig</h2>
+            <ul className={s.aboutDescText}>
+              <li>
+                Do you have your own unique logo? Would you like to make it work
+                in a new way? Brand mark animation is the latest word in video
+                marketing. I know exactly how to make your logo animation
+                recognizable.
+              </li>
+              <li>
+                You can use it for Instagram and Facebook videos and on your
+                YouTube channel to highlight your brand identity. All I need is
+                a logo in .ai format vectors files. If you don't have one,
+                that's not a problem. I can additionally convert it from .jpg or
+                .png. I also need to hear the wishes and expectations of the
+                result. So, I will ask a few questions to understand the work's
+                specifics better. Mutual communication is the way to success!
+              </li>
+              <li>
+                It is easy to order: choose the right package and contact me.
+                After a short preview and discussion of essential details, I
+                will proceed to work. In the end, I will send you the finished
+                video in .mov and .mp4 format for confirmation. You can make
+                edits or accept this version.
+              </li>
+              <li>
+                You will receive the animation and all the source files in one
+                package when the work is done. So, do not waste your time and
+                contact me for an animated logo and increase brand awareness,
+                stand out from the crowd, and extend your audience!
+              </li>
+            </ul>
+          </div>
+        </Container>
+      </section>
+      <section className={s.storeSection}>
+        <Container>
+          <div>
+            <h2>Store</h2>
+            <div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
           </div>
         </Container>
       </section>
