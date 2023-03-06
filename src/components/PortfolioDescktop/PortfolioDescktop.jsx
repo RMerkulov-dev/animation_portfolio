05b05c1a53bad04f01b0be54c990426bf73a31ac
RMerkulov <fotoromario@gmail.com>
2023-03-06 22:41:12 +0200

    upd

--- src/components/PortfolioDescktop/PortfolioDescktop.jsx
+++ src/components/PortfolioDescktop/PortfolioDescktop.jsx
@@ -1,23 +1,51 @@
 import s from "./PortfolioDesctop.module.scss";
+import { sliderTablet } from "../../helpers/cards";
 
 const PortfolioDesctop = () => {
   return (
     <section>
       <div className={s.portfolioWrapper}>
         <ul className={s.portfolioList}>
-          <li className={s.item1}></li>
-          <li className={s.item2}></li>
-          <li className={s.item3}></li>
-          <li className={s.item4}></li>
-          <li className={s.item5}></li>
-          <li className={s.item6}></li>
-          <li className={s.item7}></li>
-          <li className={s.item8}></li>
-          <li className={s.item9}></li>
-          <li className={s.item10}></li>
-          <li className={s.item11}></li>
-          <li className={s.item12}></li>
-          <li className={s.item13}></li>
+          <li className={s.item1}>
+            <img src={sliderTablet[0]} alt="card" />
+          </li>
+          <li className={s.item2}>
+            <img src={sliderTablet[1]} alt="card" />
+          </li>
+          <li className={s.item3}>
+            <img src={sliderTablet[2]} alt="card" />
+          </li>
+          <li className={s.item4}>
+            <img src={sliderTablet[3]} alt="card" />
+          </li>
+
+          <li className={s.item9}>
+            <img src={sliderTablet[4]} alt="card" />
+          </li>
+          <li className={s.item10}>
+            <img src={sliderTablet[5]} alt="card" />
+          </li>
+          <li className={s.item11}>
+            <img src={sliderTablet[6]} alt="card" />
+          </li>
+          <li className={s.item12}>
+            <img src={sliderTablet[7]} alt="card" />
+          </li>
+          <li className={s.item13}>
+            <img src={sliderTablet[8]} alt="card" />
+          </li>
+          <li className={s.item14}>
+            <img src={sliderTablet[9]} alt="card" />
+          </li>
+          <li className={s.item15}>
+            <img src={sliderTablet[11]} alt="card" />
+          </li>
+          <li className={s.item16}>
+            <img src={sliderTablet[12]} alt="card" />
+          </li>
+          <li className={s.item17}>
+            <img src={sliderTablet[13]} alt="card" />
+          </li>
         </ul>
       </div>
     </section>
--- src/helpers/cards.js
+++ src/helpers/cards.js
@@ -2,9 +2,12 @@ import Studio from "../assets/portfolio/studio.jpg";
 import Matrix from "../assets/portfolio/matrix.jpg";
 import Trance from "../assets/portfolio/trance.jpg";
 import Sound from "../assets/portfolio/sound.jpg";
+import SoundTablet from "../assets/portfolio/sound_tab.jpg";
 import Business from "../assets/portfolio/business.jpg";
 import Archer from "../assets/portfolio/archer.jpg";
 import Coolinart from "../assets/portfolio/coolinart.jpg";
+import CoolinartTab from "../assets/portfolio/coolinart_tab.jpg";
+import BoorgerTab from "../assets/portfolio/burger_tab.jpg";
 import Energy from "../assets/portfolio/energy.jpg";
 import Beauty from "../assets/portfolio/beauty.jpg";
 import Indastry from "../assets/portfolio/indastry.jpg";
@@ -24,6 +27,23 @@ export const slider = [
   Beauty,
   Indastry,
   Burger,
+  Fr,
+  Map,
+];
+
+export const sliderTablet = [
+  Studio,
+  Matrix,
+  Trance,
+  Business,
+  SoundTablet,
+  Archer,
+  Indastry,
+  CoolinartTab,
+  Energy,
+  Beauty,
+  Indastry,
+  BoorgerTab,
   Fr,
   Map,
 ];
