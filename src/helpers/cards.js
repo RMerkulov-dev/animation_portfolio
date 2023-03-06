3d4e19c3f8a4bf684ba464aa006c36d34a8b4b9e
RMerkulov <fotoromario@gmail.com>
2023-03-06 20:41:01 +0200

    upd

---
+++ src/helpers/cards.js
@@ -0,0 +1,29 @@
+import Studio from "../assets/portfolio/studio.jpg";
+import Matrix from "../assets/portfolio/matrix.jpg";
+import Trance from "../assets/portfolio/trance.jpg";
+import Sound from "../assets/portfolio/sound.jpg";
+import Business from "../assets/portfolio/business.jpg";
+import Archer from "../assets/portfolio/archer.jpg";
+import Coolinart from "../assets/portfolio/coolinart.jpg";
+import Energy from "../assets/portfolio/energy.jpg";
+import Beauty from "../assets/portfolio/beauty.jpg";
+import Indastry from "../assets/portfolio/indastry.jpg";
+import Burger from "../assets/portfolio/burger.jpg";
+import Fr from "../assets/portfolio/fr.jpg";
+import Map from "../assets/portfolio/map.jpg";
+
+export const slider = [
+  Studio,
+  Matrix,
+  Trance,
+  Sound,
+  Business,
+  Archer,
+  Coolinart,
+  Energy,
+  Beauty,
+  Indastry,
+  Burger,
+  Fr,
+  Map,
+];
