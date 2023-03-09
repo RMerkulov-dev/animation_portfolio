16cc699cbb4d8bdfe182a7f20f1d27bca0e8d9f5
RMerkulov <fotoromario@gmail.com>
2023-03-09 11:30:04 +0200

    upd

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,21 @@
 import s from "./Header.module.scss";
 import Container from "../Container/Container";
+import { smoothScroll } from "../../hooks/useScroll";
 
 const Header = () => {
+  function handleLinkClick(event, targetId) {
+    event.preventDefault();
+    const duration = 500;
+    smoothScroll(targetId, duration);
+  }
   return (
     <header className={s.header}>
       <Container>
         <nav className={s.headerNav}>
-          <a>Home</a>
-          <a>Portfolio</a>
-          <a>Contacts</a>
-          <a>Store</a>
+          <a onClick={(e) => handleLinkClick(e, "hero")}>Home</a>
+          <a onClick={(e) => handleLinkClick(e, "portfolio")}>Portfolio</a>
+          <a onClick={(e) => handleLinkClick(e, "contacts")}>Contacts</a>
+          <a onClick={(e) => handleLinkClick(e, "store")}>Store</a>
         </nav>
       </Container>
     </header>
