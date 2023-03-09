f322b938797c57eae6ad0faa57039afaaa0d9bc1
RMerkulov <fotoromario@gmail.com>
2023-03-09 14:40:25 +0200

    upd

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -4,7 +4,49 @@ import { ReactComponent as Tel } from "../../assets/icons/phone.svg";
 import { ReactComponent as Linkedin } from "../../assets/icons/linkedin.svg";
 import { ReactComponent as Telegramm } from "../../assets/icons/telegramm.svg";
 
+import emailjs from "emailjs-com";
+
 const Contacts = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_53jfxv6",
+        "template_0w8gr42",
+        e.target,
+        "YvY8uSNnL7NliQW8N"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    // Include the email address of the person who wrote the email in the email body
+    const fromName = e.target.name.value;
+    const fromEmail = e.target.email.value;
+    const message = `New message from ${fromEmail}: ${e.target.message.value}`;
+    emailjs
+      .send(
+        "service_53jfxv6",
+        "template_0w8gr42",
+        { fromName: fromName, from_email: fromEmail, message_html: message },
+        "YvY8uSNnL7NliQW8N"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className={s.contactsWrapper}>
       <div className={s.contactsMedia}>
@@ -29,16 +71,30 @@ const Contacts = () => {
 
       <div className={s.contactsMedia}>
         <h3 className={s.contactsTitle}>Contact us</h3>
-        <form className={s.contactForm}>
+        <form className={s.contactForm} onSubmit={sendEmail}>
           <label>
-            <input type="text" className={s.contactInput} />
+            <input
+              type="text"
+              name="name"
+              className={s.contactInput}
+              placeholder="Enter your name"
+            />
           </label>
           <label>
-            <input type="email" className={s.contactInput} />
+            <input
+              type="email"
+              name="email"
+              className={s.contactInput}
+              placeholder="Enter your email"
+            />
           </label>
-          <button className={s.btnSubmit} type="submit">
-            Send
-          </button>
+          <textarea
+            name="message"
+            className={s.contactInput}
+            rows={10}
+            placeholder="Add your message"
+          />
+          <input className={s.btnSubmit} type="submit" value="Send" />
         </form>
       </div>
     </div>
