bf2f4c1be10aba06321d57b51c95362d0714f00b
RMerkulov <fotoromario@gmail.com>
2023-03-10 10:17:00 +0200

    upd

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import HomePage from "./views/HomePage/HomePage";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ShoppingPage from "./views/ShoppingPage/ShoppingPage";
-import Routers from "./routers/Routers";
+import ProductDetails from "./views/ProductDetails/ProductDetails";
+import Chart from "./views/Chart/Chart";
+import Checkout from "./views/Chekout/Chekout";
+import LoginPage from "./views/LoginPage/LoginPage";
+import SignupPage from "./views/SignupPage/SignupPage";
 
 export const App = () => {
   return (
     <>
       <ToastContainer />
-      {/*<Routes>*/}
-      {/*  <Route path="/" element={<HomePage />} />*/}
-      {/*  <Route path="/shop" element={<ShoppingPage />} />*/}
-      {/*</Routes>*/}
-      <Routers />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShoppingPage />} />
+        <Route path="/shop/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Chart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
     </>
   );
 };
