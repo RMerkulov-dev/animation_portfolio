3e585c0d9667eb97b4189ff36aaa88fc1b3f985b
RMerkulov <fotoromario@gmail.com>
2024-04-29 22:28:41 +0300

    add videos

--- src/data/videos.js
+++ src/data/videos.js
@@ -15,60 +15,75 @@ import Arpanet from '../assets/video/arpanet.mp4';
 import Office from '../assets/video/office.mp4';
 import SDesign from '../assets/video/s_design.mp4';
 import F16Video from '../assets/video/f16.mp4';
-import { color } from 'framer-motion';
+import ArktikTravel from '../assets/video/arktik_travel.mp4';
+import HandWatch from '../assets/video/handwatch.mp4';
+import Kitchen from '../assets/video/kitchen.mp4';
 
 export const VIDEOS = [
   { index: '1', content: F16Video, title: 'F-16', color: '#E8DCCC' },
-  { index: '2', content: Office, title: 'Foxes Print', color: '#E1A9C4' },
-  { index: '3', content: Arpanet, title: 'ARPAnet net', color: '#FFFFFF' },
-  { index: '4', content: BurgerVideo, title: 'Burger Club', color: '#E8DCCC' },
-  { index: '5', content: SDesign, title: 'Sound Animation', color: '#e3bff2' },
-  { index: '7', content: MapVideo, title: 'Map USA', color: '#FFFFFF' },
   {
-    index: '8',
+    index: '2',
+    content: ArktikTravel,
+    title: 'Arktik Travel',
+    color: '#E1A9C4',
+  },
+  {
+    index: '3',
+    content: HandWatch,
+    title: 'Hand Watch',
+    color: '#E1A9C4',
+  },
+  { index: '4', content: Office, title: 'Foxes Print', color: '#E1A9C4' },
+  { index: '5', content: Arpanet, title: 'ARPAnet net', color: '#FFFFFF' },
+  { index: '6', content: BurgerVideo, title: 'Burger Club', color: '#E8DCCC' },
+  { index: '7', content: SDesign, title: 'Sound Animation', color: '#e3bff2' },
+  { index: '8', content: Kitchen, title: 'Kitchen', color: '#e3bff2' },
+  { index: '9', content: MapVideo, title: 'Map USA', color: '#FFFFFF' },
+  {
+    index: '10',
     content: IndustryVideo,
     title: 'Industry Corporation',
     color: '#E1A9C4',
   },
   {
-    index: '9',
+    index: '11',
     content: ArcherVideo,
     title: 'Archer Health',
     color: '#87f5be',
   },
   {
-    index: '10',
+    index: '12',
     content: CoolinartVideo,
     title: 'Coolirant',
     color: '#ABABD1',
   },
-  { index: '11', content: BusinessVideo, title: 'Business', color: '#E8B099' },
+  { index: '13', content: BusinessVideo, title: 'Business', color: '#E8B099' },
   {
-    index: '12',
+    index: '14',
     content: StudioVideo,
     title: 'Recording studio Saxophon',
     color: '#FFFFFF',
   },
   {
-    index: '13',
+    index: '15',
     content: MusicVideo,
     title: 'Music Project',
     color: '#E8DCCC',
   },
   {
-    index: '14',
+    index: '16',
     content: TranceVideo,
     title: 'Michael Keane',
     color: '#E8B099',
   },
   {
-    index: '15',
+    index: '17',
     content: EnergyVideo,
     title: 'Energy Green',
     color: '#E1A9C4',
   },
   {
-    index: '16',
+    index: '18',
     content: MatrixVideo,
     title: 'Matrix Company',
     color: '#E8DCCC',
